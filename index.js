65404091ba369f06d3019463da13b97d143e58a3
Makhmadkulov Bobur <boburmakhmadkulov05@gmail.com>
2024-02-23 12:16:37 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,49 @@
+let form = document.forms.sign_up
+let inputs = form.querySelectorAll('input')
+let successful_fields = document.querySelector('#success')
+let error_fields = document.querySelector('#error')
+let modal = document.querySelector('#modal')
+let close_modal = document.querySelector('#modal button')
+let body = document.body
+
+form.onsubmit = (event) => {
+    event.preventDefault()
+    let error = false
+    let count_error = 0
+    let count_success = 7
+
+    inputs.forEach((inp) => {
+        let isRequired = inp.parentNode.classList.contains('req')
+
+        if (isRequired && inp.value.length === 0) {
+            error = true
+            inp.parentNode.classList.add('error', 'warning')
+            count_error++
+            error_fields.innerHTML = 'Error: ' + count_error + '/7'
+            count_success--
+            successful_fields.innerHTML = 'Success: ' + count_success + '/7'
+        } else {
+            inp.parentNode.classList.remove('error')
+        }
+    })
+
+    if (error) {
+        modal.style.display = 'flex'
+        body.style.overflowY = 'hidden'
+
+        close_modal.onclick = () => {
+            modal.style.display = 'none'
+            body.style.overflowY = 'auto'
+        }
+        
+        return
+    }
+    submit(event.target)
+}
+
+function submit(form) {
+    let fm = new FormData(form)
+    let user = {}
+    fm.forEach((val, key) => user[key] = val)
+    console.log(user)
+}
